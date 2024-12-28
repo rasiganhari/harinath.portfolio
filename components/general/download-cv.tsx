1f2559cff8774bb434a7db2df4e787a4d5dc939a
Harinath M <30649309+MHarinath@users.noreply.github.com>
2024-12-29 00:16:53 +0530

    updated cv

--- components/general/download-cv.tsx
+++ components/general/download-cv.tsx
@@ -4,7 +4,7 @@ import Button from '../../components/general/button';
 
 const DownloadCV = () => {
   return (
-    <Button onClick={() => window?.open('/files/HarinathMnew.pdf', '_blank')}>
+    <Button onClick={() => window?.open('/files/HarinathM-FullStackDeveloper.pdf', '_blank')}>
       Download CV
     </Button>
   );
