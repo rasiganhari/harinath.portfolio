4ca711cddc5fa666c3de4907cd972b179aec191e
Harinath M <30649309+rasiganhari@users.noreply.github.com>
2026-08-24 19:13:54 +0530

    fix(deps): upgrade Next.js to latest secure version and update asset imports

--- components/sections/about-me.tsx
+++ components/sections/about-me.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 
-import HariFullPose from '/public/images/hari-full.jpg';
+import HariFullPose from '../../public/images/hari-full.jpg';
 import Tag from '../../components/data-display/tag';
 import Container from '../../components/layout/container';
 import Typography from '../../components/general/typography';
--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import { MapPin } from 'lucide-react';
 
-import HariHeadshot from '/public/images/hari-1.jpg';
+import HariHeadshot from '../../public/images/hari-1.jpg';
 import SocialIcons from '../../components/data-display/social-icons';
 import Typography from '../../components/general/typography';
 import Container from '../../components/layout/container';
